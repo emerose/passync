a5de2e487c49eee88ab60b5f7478f71b5c7492a2
sam quigley <sq@sqmbp.local>
2020-08-06 19:32:20 -0400

    AgileKeychain: fail on nonexistent directories

--- agilekeychain/agilekeychain.go
+++ agilekeychain/agilekeychain.go
@@ -1,5 +1,11 @@
 package agilekeychain
 
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
 // AgileKeychain represents a 1password AgileKeychain
 // see design discussion here: https://support.1password.com/cs/agile-keychain-design/
 type AgileKeychain struct {
@@ -7,10 +13,25 @@ type AgileKeychain struct {
 }
 
 // NewAgileKeychain creates a new AgileKeychain object, given a path
-// NOTE: this does no validation on the path or its contents!
-func NewAgileKeychain(path string) (*AgileKeychain, error) {
+// returns an error if path doesn't exist
+func NewAgileKeychain(keychainPath string) (*AgileKeychain, error) {
+	if !path.IsAbs(keychainPath) {
+		dir, err := os.Getwd()
+
+		if err != nil {
+			return nil, fmt.Errorf("Couldn't get current dir: %v", err)
+		}
+
+		keychainPath = path.Join(dir, keychainPath)
+	}
+
 	ret := &AgileKeychain{
-		baseDir: path,
+		baseDir: keychainPath,
+	}
+
+	_, err := os.Stat(keychainPath)
+	if os.IsNotExist(err) {
+		return nil, fmt.Errorf("Non-existent AgileKeychain path %s: %v", keychainPath, err)
 	}
 
 	return ret, nil
--- agilekeychain/agilekeychain_test.go
+++ agilekeychain/agilekeychain_test.go
@@ -1,11 +1,13 @@
 package agilekeychain
 
 import (
+	"os"
+	"path"
 	"reflect"
 	"testing"
 )
 
-func TestNewAgileKeychain(t *testing.T) {
+func TestNewAgileKeychain_Errors(t *testing.T) {
 	type args struct {
 		path string
 	}
@@ -15,11 +17,11 @@ func TestNewAgileKeychain(t *testing.T) {
 		want    *AgileKeychain
 		wantErr bool
 	}{
-		{ // this fixture shamelessly copied from https://github.com/alsemyonov/one_password
-			name:    "Create new agilekeychain from fixture",
-			args:    args{path: "testdata/agilekeychain/example/1Password.agilekeychain"},
-			want:    &AgileKeychain{baseDir: "testdata/agilekeychain/example/1Password.agilekeychain"},
-			wantErr: false,
+		{
+			name:    "Test nonexistent directory",
+			args:    args{path: "/nonexist4329489erjgar"},
+			want:    nil,
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
@@ -34,4 +36,30 @@ func TestNewAgileKeychain(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestNewAgileKeychain_Example1(t *testing.T) {
+	// this fixture shamelessly copied from https://github.com/alsemyonov/one_password
+	fixturePath := "../testdata/agilekeychain/example1/1Password.agilekeychain"
+
+	keychain1, err := NewAgileKeychain(fixturePath)
+	if err != nil {
+		t.Errorf("Error creating agilekeychain from fixture with relative path: %v", err)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Errorf("os.Getwd failed: %v", err)
+	}
+
+	absPath := path.Join(cwd, fixturePath)
+	keychain2, err := NewAgileKeychain(absPath)
+	if err != nil {
+		t.Errorf("Error creating agilekeychain from fixture with absolute path: %v", err)
+	}
+
+	if !reflect.DeepEqual(keychain1, keychain2) {
+		t.Errorf("Keychains from absolute and relative paths differ! relative: %v absolute: %v", keychain1, keychain2)
+	}
+
 }
