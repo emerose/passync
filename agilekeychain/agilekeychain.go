5ea7488c08f9ac86c574dd111e4f546ef63905b8
sam quigley <sq@sqmbp.local>
2020-08-07 21:58:41 -0400

    A bit surprised, but this seems to work

--- agilekeychain/agilekeychain.go
+++ agilekeychain/agilekeychain.go
@@ -1,10 +1,21 @@
 package agilekeychain
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"os"
 	"path"
+	"strings"
+
+	"golang.org/x/crypto/pbkdf2"
 )
 
 // AgileKeychain represents a 1password AgileKeychain
@@ -55,6 +66,11 @@ func NewAgileKeychain(keychainPath string) (*AgileKeychain, error) {
 	}
 
 	err = ret.loadContents()
+	if err != nil {
+		return nil, err
+	}
+
+	err = ret.loadEncryptionKeys("1Password")
 	if err != nil {
 		return nil, err
 	}
@@ -121,7 +137,185 @@ func (k *AgileKeychain) loadContents() error {
 	}
 
 	k.contents = cookedContents
-	return err
+	return nil
+}
+
+func (k *AgileKeychain) loadEncryptionKeys(passphrase string) error {
+	type rawEncryptionKey struct {
+		Data       string
+		Validation string
+		Level      string
+		Identifier string
+		Iterations int
+	}
+
+	type rawEncryptionKeys struct {
+		SL3  string
+		SL5  string
+		List []rawEncryptionKey
+	}
+
+	contentsPath := path.Join(k.baseDir, "data", "default", "encryptionKeys.js")
+	f, err := os.Open(contentsPath)
+	if err != nil {
+		return err
+	}
+
+	var raw rawEncryptionKeys
+
+	err = json.NewDecoder(f).Decode(&raw)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Found %d keys", len(raw.List))
+
+	for _, key := range raw.List {
+		// these strings end in "\u0000" which makes for some invalid base64
+		if strings.HasSuffix(key.Data, "\u0000") {
+			key.Data = key.Data[0 : len(key.Data)-len("\u0000")]
+		}
+		if strings.HasSuffix(key.Validation, "\u0000") {
+			key.Validation = key.Validation[0 : len(key.Validation)-len("\u0000")]
+		}
+
+		data, err := base64.StdEncoding.DecodeString(key.Data)
+		if err != nil {
+			return err
+		}
+		validation, err := base64.StdEncoding.DecodeString(key.Validation)
+		if err != nil {
+			return err
+		}
+
+		_, err = decryptKey(data, validation, key.Iterations, passphrase)
+	}
+	return nil
+}
+
+func decryptKey(dataBytes []byte, validationBytes []byte, iterations int, passphrase string) ([]byte, error) {
+	var salt []byte
+
+	// if the data starts with "Salted__", then the first 8 bytes following that are the salt for PBKDF2
+	if bytes.Equal(dataBytes[0:8], []byte(`Salted__`)) {
+		salt = dataBytes[8:16]
+	} else {
+		salt = []byte{0, 0, 0, 0, 0, 0, 0, 0}
+	}
+
+	// encrypted key bytes
+	encryptedKey := dataBytes[16:]
+
+	derivedKey := pbkdf2.Key([]byte(passphrase), salt, iterations, 32, sha1.New)
+
+	// the key-encrypting key
+	kek := derivedKey[0:16]
+	// and associated IV
+	iv := derivedKey[16:32]
+
+	block, err := aes.NewCipher(kek)
+	if err != nil {
+		return nil, err
+	}
+
+	decrypter := cipher.NewCBCDecrypter(block, iv)
+	if err != nil {
+		return nil, err
+	}
+
+	key := make([]byte, len(encryptedKey))
+	decrypter.CryptBlocks(key, encryptedKey)
+
+	key, err = unpad(key, decrypter.BlockSize())
+	if err != nil {
+		return nil, err
+	}
+
+	// now, validate it
+
+	// if the data starts with "Salted__", then the first 8 bytes following that are the salt for PBKDF2
+	if bytes.Equal(validationBytes[0:8], []byte(`Salted__`)) {
+		salt = validationBytes[8:16]
+	} else {
+		salt = []byte{0, 0, 0, 0, 0, 0, 0, 0}
+		return nil, fmt.Errorf("unsalted validation data not implemented")
+	}
+
+	encryptedBytes := validationBytes[16:]
+
+	kek, iv = deriveKey(key, salt)
+
+	block, err = aes.NewCipher(kek)
+	if err != nil {
+		return nil, err
+	}
+
+	decrypter = cipher.NewCBCDecrypter(block, iv)
+	if err != nil {
+		return nil, err
+	}
+
+	validationResult := make([]byte, len(encryptedBytes))
+	decrypter.CryptBlocks(validationResult, encryptedBytes)
+
+	validationResult, err = unpad(validationResult, decrypter.BlockSize())
+	if err != nil {
+		return nil, err
+	}
+
+	if !bytes.Equal(key, validationResult) {
+		return nil, errors.New("failed to validate key")
+	}
+
+	return key, nil
+}
+
+// unpad is needed because this is how openssl pads aes-128-cbc, so we
+// need to unpad as well in order to properly decrypt the data. Note
+// that this should conform to PCKS#7.
+func unpad(data []byte, blocksize int) ([]byte, error) {
+	if blocksize <= 0 {
+		return nil, errors.New("Invalid block size")
+	}
+
+	if data == nil || len(data) == 0 {
+		return nil, errors.New("Invalid data")
+	}
+
+	if len(data)%blocksize != 0 {
+		return nil, errors.New("Input is not a multiple of blocksize")
+	}
+
+	lastByte := data[len(data)-1]
+	padSize := int(lastByte)
+	if padSize == 0 || padSize > len(data) {
+		return nil, errors.New("Invalid pad size")
+	}
+
+	padding := data[len(data)-padSize:]
+	for _, b := range padding {
+		if b != lastByte {
+			return nil, errors.New("Invalid padding")
+		}
+	}
+
+	return data[:len(data)-padSize], nil
+}
+
+func deriveKey(password []byte, salt []byte) (key []byte, iv []byte) {
+	rounds := 2
+	data := append(password, salt...)
+	md5Hashes := make([][]byte, rounds)
+	sum := md5.Sum(data)
+
+	md5Hashes[0] = append([]byte{}, sum[:]...)
+
+	for i := 1; i < rounds; i++ {
+		sum = md5.Sum(append(md5Hashes[i-1], data...))
+		md5Hashes[i] = append([]byte{}, sum[:]...)
+	}
+
+	return md5Hashes[0], md5Hashes[1]
 }
 
 // Length of the keychain
